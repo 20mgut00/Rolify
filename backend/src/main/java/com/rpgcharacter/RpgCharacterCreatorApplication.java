4a2fe89fc594d1dc9a6a9ad6034034414fc3cd57
Miguel <miguelgutierrez20@gmail.com>
2026-02-26 16:39:52 +0100

    fix verification email

--- backend/src/main/java/com/rpgcharacter/RpgCharacterCreatorApplication.java
+++ backend/src/main/java/com/rpgcharacter/RpgCharacterCreatorApplication.java
@@ -3,9 +3,11 @@ package com.rpgcharacter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableMongoAuditing
+@EnableAsync
 public class RpgCharacterCreatorApplication {
 
     public static void main(String[] args) {
